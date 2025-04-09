4d2287d04aff32d48346d5068e912b4c326e29dc
drdon1234 <96719161+drdon1234@users.noreply.github.com>
2025-04-10 00:40:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+# from astrbot.api import logger
 from .utils.config_manager import load_config
 from .utils.downloader import Downloader
 from .utils.html_parser import HTMLParser
